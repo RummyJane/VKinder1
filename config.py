d16b02a8813407c544c3dd2fa2003e24b7eeba16
Ioanna Yakovleva <113993855+RummyJane@users.noreply.github.com>
2022-12-25 10:22:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,1 @@
-''' строка запроса https://oauth.vk.com/oauth/authorize?client_id=51498475&display=page&scope=stats,offline,photos,status&response_type=token&v=5.131
-    '''
 
-''' id и ключ приложения для запросов к VK_API
-    APP_ID = 51498475'''
-access_token = 'vk1.a.Az7VaNansjxZxXGbXGMoaSBwoBafE-PECN5vGYPm55aSWNIr3lKbtvx5I2nEp9-KGwbv4C7tSTV-VtaaVRj74lENAbsOanq3Xk7mSru24hLkq_6PjSlCCE5mWmpB98eNysDd16FDmUHmXlMm2zqByPoCOHww83qvqIoBkLOF4n1C0d4B5eN36Yh7Zbfz3slpP1jwoZ4tkTgmZIga6_54BA'
-
-''' токен для бота из группы Интерес, чтобы отправлять сообщения и получать их''' 
-token = 'vk1.a.DL2Cx98H-5gTMCcTNEzmRLJymAI_IcaU3CqpMoB9RFTJ2E4ZHHEGQAUb-Vu-CWqafpwzUuTof6cda7NGqB5xG9Dz-EalE2W1JAq_JPgyapQVC0EG23HrjzLuIGruR3AiQQx-2CLISRwt1cUmGGc9tkrCE795l_9JYfVF06Rlp6Gk6LPyEqBAwFJ2ktlVfacRQzhmBPPUKVlENRUcsUpg5g'
-
-'''Пароль для postgres'''
-password='yana22'
